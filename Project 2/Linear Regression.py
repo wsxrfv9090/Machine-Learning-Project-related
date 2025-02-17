3312081d309384a42d8b777d154ed360dc35d77c
Davy <wsxrfv9090davyyu@gmail.com>
2025-02-17 23:53:03 +0800

    Added ipynb file for project 2, including explanations.

--- Project 2/Linear Regression.py
+++ Project 2/Linear Regression.py
@@ -1,13 +1,19 @@
 import pandas as pd
-import math
+import math, datetime
 import numpy as np
 from sklearn import preprocessing, model_selection
 from sklearn.linear_model import LinearRegression
-import joblib
+# Use to plot the data
+import matplotlib.pyplot as plt
+# Use to make data look better
+from matplotlib import style
+
+# Use to specify the style of the plot
+style.use('ggplot')
 
 
 #Selecting files
-file_path = r'D:\ImportanFiles\Coding Related\Repositories\Machine Learning project related\Project 2\Data\002796_metadata_utf8.csv'
+file_path = r'Project 2\Data\600873_metadata_utf8.csv'
 stock_df = pd.read_csv(file_path)
 
 #Calculating high-low percentage and percentage change
@@ -17,6 +23,9 @@ stock_df['PCT_change'] = (stock_df['Closing Price'] - stock_df['Opening Price'])
 #Drop useless data
 stock_df = stock_df[['Closing Price', 'HL_PCT', 'PCT_change', 'Trading Volume']]
 
+# Remove commas and convert to numeric, since in metadata the trading volume contain commas
+stock_df['Trading Volume'] = stock_df['Trading Volume'].replace({',': ''}, regex=True).astype(float)
+
 #Choose which data column is the one that will be predicted
 forecast_col = 'Closing Price'
 
@@ -25,32 +34,33 @@ forecast_col = 'Closing Price'
 
 #Give a row number to shift, in this case: len(stock_df) = 6884, math.ceil make it 7, then type convert to integer, meaning that later .shift function will shift closing price upwards 7 rows and fill them into Label rows. Basically we are using the datas to predict the closing price 7 days later
 forecast_out = int(math.ceil(0.001*len(stock_df)))
+
+#Anwers
 stock_df['Label'] = stock_df[forecast_col].shift(-forecast_out)
 print(forecast_out)
 
-stock_df.dropna(inplace = True)
-
-# Remove commas and convert to numeric, since in metadata the trading volume contain commas
-stock_df['Trading Volume'] = stock_df['Trading Volume'].replace({',': ''}, regex=True).astype(float)
-
-
-
+# Split the where X is the features and y is the answer
 X = np.array(stock_df.drop(['Label'], axis = 1))
+X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
+stock_df.dropna(inplace = True)
 y = np.array(stock_df['Label'])
 
-X = preprocessing.scale(X)
+# Scale the data
 
+# Split the data, 80% for training and 20% for testing (randomly)
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size = 0.2)
 
-print(X_train.mean())
-print(X_train.std())
-
+# Getting the classifier
 clf = LinearRegression(n_jobs = -1)
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
 
-# Save the trained model to a file
-#joblib.dump(clf, 'stock_prediction_model.pkl')
+forecast_set = clf.predict(X_lately)
 
-print(accuracy)
+print("Forecast result: " + str(forecast_set) + " with accuracy of " + str(accuracy) + " and forecast out of " + str(forecast_out) + " days.")
+
+stock_df['Forecast'] = np.nan
 
---
+++ Project 2/readme.md
@@ -0,0 +1,4 @@
+# Recourse
+https://www.youtube.com/watch?v=QLVMqwpOLPk&list=PLQVvvaa0QuDfKTOs3Keq_kaG2P55YRn5v&index=5
+
+The course's first project it this linear regression part, where it gives you a outlook of what a project looks like.
